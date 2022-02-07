c3b8d3b1dd2556a684e0888a9476bd805571dbda
HweeVaa <gnlqkgnlgus@gmail.com>
2022-02-07 16:47:22 +0900

    second commit

--- DJANGO_LEVEL_ONE/My_Django_Stuff/ProTwo/AppTwo/views.py
+++ DJANGO_LEVEL_ONE/My_Django_Stuff/ProTwo/AppTwo/views.py
@@ -3,7 +3,12 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("<em>My Second App </em>")
+    return HttpResponse("<em> My Second Project</em>")
+
+
+def help(request):
+    helpdict = {"help_insert": "HELP PAGE"}
+    return render(request, "AppTwo/help.html", context=helpdict)
 
 
 # Create your views here.
