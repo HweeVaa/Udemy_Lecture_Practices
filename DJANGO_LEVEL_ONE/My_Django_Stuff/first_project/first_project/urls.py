ecc04cc5f000b357715e4a97273146130102b1aa
HweeVaa <gnlqkgnlgus@gmail.com>
2022-02-09 14:05:48 +0900

    second commit

--- DJANGO_LEVEL_ONE/My_Django_Stuff/first_project/first_project/urls.py
+++ DJANGO_LEVEL_ONE/My_Django_Stuff/first_project/first_project/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from first_app import views
 
+# 강의에서는 django.conf.urls를 썼으나 현재는 django.urls로 바뀐 것 같다.
 # 강의에서는 url 함수를 썼으나, 삭제되고 path로 대체된 듯 하다.
 urlpatterns = [
     path("", views.index),
+    path("first_app/", include("first_app.urls")),
     path("admin/", admin.site.urls),
 ]
