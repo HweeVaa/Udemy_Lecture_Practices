18bb32367e2a47462ab3070d03ec24097716e267
HweeVaa <gnlqkgnlgus@gmail.com>
2022-02-14 14:18:13 +0900

    3rd commit

--- DJANGO_LEVEL_ONE/My_Django_Stuff/first_project/first_app/admin.py
+++ DJANGO_LEVEL_ONE/My_Django_Stuff/first_project/first_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from first_app.models import AccesssRecord, Topic, Webpage
 
 # Register your models here.
+
+admin.site.register(AccesssRecord)
+admin.site.register(Topic)
+admin.site.register(Webpage)
--- DJANGO_LEVEL_ONE/My_Django_Stuff/first_project/first_app/models.py
+++ DJANGO_LEVEL_ONE/My_Django_Stuff/first_project/first_app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Topic(models.Model):
+    top_name = models.CharField(max_length=264, unique=True)
+
+    def __str__(self):
+        return self.top_name
+
+
+# Django 2 이상부터는 ForeignKey 기능을 사용할 때 on_delete에 해당되는 기능을 추가해줘야 한다.
+class Webpage(models.Model):
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    name = models.CharField(max_length=264, unique=True)
+    url = models.URLField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class AccesssRecord(models.Model):
+    name = models.ForeignKey(Webpage, on_delete=models.CASCADE)
+    date = models.DateField()
+
+    def __str__(self):
+        return str(self.date)
